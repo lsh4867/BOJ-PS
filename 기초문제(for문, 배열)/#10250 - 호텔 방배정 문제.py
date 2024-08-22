aea699d2d993b07406bc9cf559695244b5505d74
Seunghwan Lee <80394375+lsh4867@users.noreply.github.com>
2024-08-22 17:21:02 +0900

    Update #10250 - 호텔 방배정 문제.py

--- 기초문제(for문, 배열)/#10250 - 호텔 방배정 문제.py
+++ 기초문제(for문, 배열)/#10250 - 호텔 방배정 문제.py
@@ -15,5 +15,5 @@ n = int(input())
 for _ in range(n):
     a, b, c = map(int, input().split())
     floor = c % a if c % a != 0 else a
-    num = (c-1)//a + 1 # c-1 : 0번 인덱스부터 시작하는 개념 적용(마지막 +1로 복구)
+    num = (c-1)//a + 1 # c-1 : 0 기반 인덱스로 조정(마지막 +1로 실제 값 도출)
     print(f'{floor}{num:02}') # 두 자리 맞추기 method (format 함수보다 단순함!!)
